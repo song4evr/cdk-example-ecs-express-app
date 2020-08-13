59c97af3297315d6c42d0b96e66adaf94abcf88c
Song Jin <sonjin@amazon.com>
2020-08-13 16:39:34 +0000

    Delete test cases

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,5 +1,5 @@
 import cdk = require("@aws-cdk/core");
-import ecr = require("@aws-cdk/aws-ecr");
+//import ecr = require("@aws-cdk/aws-ecr");
 import ecs = require("@aws-cdk/aws-ecs");
 import ec2 = require("@aws-cdk/aws-ec2");
 import ecsPatterns = require("@aws-cdk/aws-ecs-patterns");
